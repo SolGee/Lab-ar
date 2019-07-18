bbf5fe55fe905353400340f7b6a1dadd6c7118d3
SolGee <solsv00@hotmail.com>
2019-07-18 17:31:56 -0500

    Se estilizo la primer sección version desktop

---
+++ app.js
@@ -0,0 +1,10 @@
+// HIDE & SHOW
+
+const hideSection = id => document.getElementById(id).classList.add('hide');
+const showSection = id => document.getElementById(id).classList.remove('hide');
+
+// MODALS
+
+const startSession = document.getElementById('new-session');
+const account = document.getElementById('account');
+const closeModal = ;
